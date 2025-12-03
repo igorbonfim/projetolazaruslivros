4f645efc1a5a7e5ae50f4ea9538b93d22b729cf3
Igor Bonfim <igorbonfim88@gmail.com>
2025-12-02 23:49:06 -0300

    Inicio do projeto

---
+++ backup/uprincipal.pas
@@ -0,0 +1,30 @@
+unit uPrincipal;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls;
+
+type
+
+  { TFrmPrincipal }
+
+  TFrmPrincipal = class(TForm)
+    pnlTopo: TPanel;
+  private
+
+  public
+
+  end;
+
+var
+  FrmPrincipal: TFrmPrincipal;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
---
+++ uprincipal.pas
@@ -0,0 +1,31 @@
+unit uPrincipal;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, EditBtn,
+  Buttons;
+
+type
+
+  { TFrmPrincipal }
+
+  TFrmPrincipal = class(TForm)
+    pnlTopo: TPanel;
+  private
+
+  public
+
+  end;
+
+var
+  FrmPrincipal: TFrmPrincipal;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
